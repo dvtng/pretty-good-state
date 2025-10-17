94cbf4bbf8874eb1e4b6b549010a52e30b255fae
David Tang <tang.ddt@gmail.com>
2025-10-17 08:15:48 -0700

    Add test for replacing runInComponent in tests

--- pretty-good-state/src/__tests__/core.test.ts
+++ pretty-good-state/src/__tests__/core.test.ts
@@ -1,12 +1,13 @@
 import { snapshot, subscribe } from "valtio";
 import { expect, test, mock } from "bun:test";
-import { defineState } from "../core";
+import { defineState, runInComponent } from "../core";
+import { createContext, useContext } from "react";
 
-const State = defineState({
-  list: { items: [1, 2, 3] },
-});
+test("mutating from a snapshot pointer", () => {
+  const State = defineState({
+    list: { items: [1, 2, 3] },
+  });
 
-test("mutating from a snapshot", () => {
   const state = State();
 
   const subscriberMock = mock(() => {});
@@ -17,4 +18,26 @@ test("mutating from a snapshot", () => {
   expect(subscriberMock).toHaveBeenCalledTimes(1);
   snapshot(state).list.items.$().push(5);
   expect(subscriberMock).toHaveBeenCalledTimes(2);
+});
+
+test("replacing runInComponent functions in tests", () => {
+  const AnalyticsContext = createContext({ track: () => {} });
+  const State = defineState({
+    getAnalytics: runInComponent(() => {
+      return useContext(AnalyticsContext);
+    }),
+    submit() {
+      this.getAnalytics().track("submit");
+    },
+  });
+
+  const trackMock = mock(() => {});
+  const state = State((state) => {
+    state.getAnalytics = () => {
+      return { track: trackMock };
+    };
+  });
+
+  state.submit();
+  expect(trackMock).toHaveBeenCalledWith("submit");
 });
